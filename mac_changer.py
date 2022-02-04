da9348460cc6f59eb42df013dcdcf805a2635412
ASWIN A K <007aswinak007@gmail.com>
2022-02-04 14:12:19 -0400

    Error handling

--- mac_changer.py
+++ mac_changer.py
@@ -1,11 +1,29 @@
 import subprocess
+from optparse import OptionParser
 
-interface = input("interface > ")
-new_mac = input("new mac > ")
+def get_arguments():
+    parser = OptionParser()
+    parser.add_option("-i","--interface",dest="interface",help="interface to change the mac address")
+    parser.add_option("-m","--mac",dest="new_mac",help="new mac address")
+    (options,arguments)=parser.parse_args()
+    if not options.interface:
+        parser.error("[-]Please specify the interface...For more informatioon use --help")
+    if not options.new_mac:
+        parser.error("[-]Please specify the mac_address...For more informatioon use --help")
+    return options      
 
-print("[+]Changing mac address for " + interface + " to " + new_mac)
+def change_mac(interface, new_mac):
+    
+    print("[+]Changing mac address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig" , interface , "down"])
+    subprocess.call(["ifconfig" , interface , "hw",  "ether", new_mac])
+    subprocess.call(["ifconfig" , interface , "up"]) 
+    subprocess.call(["ifconfig","wlan0"])
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " +interface + " hw ether " + new_mac,shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-subprocess.call("ifconfig "+interface, shell=True)
+(options)=get_arguments()
+change_mac(options.interface,options.new_mac)
+
+
+
+
+
