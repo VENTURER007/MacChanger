725abe5514f0a902b58430de1275b4ea50146436
ASWIN A K <007aswinak007@gmail.com>
2022-02-07 08:45:52 -0400

    exeception handling fixed

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,6 @@
 import subprocess
 from optparse import OptionParser
+import re
 
 def get_arguments():
     parser = OptionParser()
@@ -18,10 +19,38 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig" , interface , "down"])
     subprocess.call(["ifconfig" , interface , "hw",  "ether", new_mac])
     subprocess.call(["ifconfig" , interface , "up"]) 
-    subprocess.call(["ifconfig","wlan0"])
 
+
+def get_current_mac(interface):
+    output = subprocess.check_output(["ifconfig",interface])
+    
+    
+    try:
+        mac_address_search_result = re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(output)).group(0)
+        return mac_address_search_result
+    except AttributeError as e:
+        print("Input is not valid")
+    except subprocess.CalledProcessError as e:
+        print("Input is not valid")
+    except Exception as e:
+        print("Something went wrong!")
+
+
+   
 (options)=get_arguments()
+(Orginal_MAC)=get_current_mac(options.interface)
+print("current MAC is " + Orginal_MAC)
+
 change_mac(options.interface,options.new_mac)
+
+(current_mac)=get_current_mac(options.interface)
+
+if options.new_mac == current_mac:
+    print("[+]MAC address changed sucessfully to " + current_mac)
+else:
+    print("[-]MAC address is not changed! ")
+
+
 
 
 
